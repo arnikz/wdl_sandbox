924290d474b4f73683d62d86d6c0cc433a0edf42
= <arnold.kuzniar@gmail.com>
2022-05-30 16:58:19 +0200

    Replace java commands with tool wrappers.

--- bin/run_ecoli_local_with_docker.sh
+++ bin/run_ecoli_local_with_docker.sh
@@ -1,6 +1,6 @@
 echo Creating Input JSON File
 python bin/create_inputs.py wdl/ecoli_base.json local > wdl/ecoli_local.json
 echo Validating WDL
-java -jar womtool-*.jar validate wdl/ecoli.wdl
+womtool validate wdl/ecoli.wdl
 echo Running Pipeline via Cromwell Local
-java -jar cromwell-*.jar run wdl/ecoli.wdl -i wdl/ecoli_local.json
+cromwell run wdl/ecoli.wdl -i wdl/ecoli_local.json
